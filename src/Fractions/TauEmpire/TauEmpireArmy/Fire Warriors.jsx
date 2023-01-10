0ef61643d701a70c976cb8f3feaa54c439d92f96
Gvyganovsky <gvyganovsky@gmail.com>
2023-01-10 10:54:30 +0300

    Strat_FireWarrior_one

--- src/Fractions/TauEmpire/TauEmpireArmy/Fire Warriors.jsx
+++ src/Fractions/TauEmpire/TauEmpireArmy/Fire Warriors.jsx
@@ -62,7 +62,7 @@ const FireWarriors = () => {
 
             <div className={style.combat_unit_param_block}>
               <img src={Ld} alt="Ld" className={style.param_img} />
-              <div className={style.param_number}>6</div>
+              <div className={style.param_number}>7</div>
             </div>
 
             <div className={style.combat_unit_param_block}>
@@ -77,7 +77,7 @@ const FireWarriors = () => {
         <div className={style.line}></div>
         <div className={style.content_title}>Оружие</div>
         <div className={style.weapon}>
-          <div className={style.gun_title}>Marker light</div>
+          <div className={style.gun_title}>Pulse rifle</div>
 
           <div className={style.weapon_content}>
             <img src={range} alt="range" className={style.param_img} />
@@ -85,42 +85,13 @@ const FireWarriors = () => {
           </div>
 
           <div className={style.weapon_content}>
-            <div className={style.gun_number}>Heavy</div>
-            <div className={style.gun_number}>1"</div>
+            <div className={style.gun_number}>Rapid Fire</div>
+            <div className={style.gun_number}>1</div>
           </div>
 
           <div className={style.weapon_content}>
             <img src={S} alt="S" className={style.param_img} />
-            <div className={style.gun_number}>-</div>
-          </div>
-
-          <div className={style.weapon_content}>
-            <div className={style.gun_number}>AP</div>
-            <div className={style.gun_number}>-</div>
-          </div>
-
-          <div className={style.weapon_content}>
-            <div className={style.gun_number}>D</div>
-            <div className={style.gun_number}>-</div>
-          </div>
-        </div>
-
-        <div className={style.weapon}>
-          <div className={style.gun_title}>Missile pod</div>
-
-          <div className={style.weapon_content}>
-            <img src={range} alt="range" className={style.param_img} />
-            <div className={style.gun_number}>36"</div>
-          </div>
-
-          <div className={style.weapon_content}>
-            <div className={style.gun_number}>Assault</div>
-            <div className={style.gun_number}>2</div>
-          </div>
-
-          <div className={style.weapon_content}>
-            <img src={S} alt="S" className={style.param_img} />
-            <div className={style.gun_number}>7</div>
+            <div className={style.gun_number}>5</div>
           </div>
 
           <div className={style.weapon_content}>
@@ -130,7 +101,7 @@ const FireWarriors = () => {
 
           <div className={style.weapon_content}>
             <div className={style.gun_number}>D</div>
-            <div className={style.gun_number}>D3</div>
+            <div className={style.gun_number}>1</div>
           </div>
         </div>
 
@@ -139,7 +110,7 @@ const FireWarriors = () => {
 
           <div className={style.weapon_content}>
             <img src={range} alt="range" className={style.param_img} />
-            <div className={style.gun_number}>18"</div>
+            <div className={style.gun_number}>24"</div>
           </div>
 
           <div className={style.weapon_content}>
@@ -191,64 +162,66 @@ const FireWarriors = () => {
             <div className={style.gun_number}>1</div>
           </div>
         </div>
+      </section>
 
-        <div className={style.weapon}>
-          <div className={style.gun_title}>Smart missile system</div>
-
-          <div className={style.weapon_content}>
-            <img src={range} alt="range" className={style.param_img} />
-            <div className={style.gun_number}>30"</div>
+      <section className={style.content}>
+        <div className={style.line}></div>
+        <div className={style.content_title}>Стратагемы</div>
+        <div className={style.strat}>
+          <div className={style.stratName_9k}>
+            <span>СОСРЕДОТОЧЕННЫЙ ОГОНЬ</span>
+            <span>2 CP</span>
           </div>
-
-          <div className={style.weapon_content}>
-            <div className={style.gun_number}>Heavy</div>
-            <div className={style.gun_number}>4</div>
-          </div>
-
-          <div className={style.weapon_content}>
-            <img src={S} alt="S" className={style.param_img} />
-            <div className={style.gun_number}>5</div>
-          </div>
-
-          <div className={style.weapon_content}>
-            <div className={style.gun_number}>AP</div>
-            <div className={style.gun_number}>0</div>
-          </div>
-
-          <div className={style.weapon_content}>
-            <div className={style.gun_number}>D</div>
-            <div className={style.gun_number}>1</div>
+          <div className={style.stratName_info}>
+            Используйте эту стратегию на этапе стрельбы, когда вражеская модель теряет какие-либо
+            ранения в результате атаки, произведенной моделью T'AU SEPT из вашей армии. До конца фазы,
+            каждый раз, когда базовая модель T'AU SEPT из вашей армии совершает атаку против подразделения
+            этой вражеской модели, добавляйте 1 к броску ранений этой атаки.
           </div>
         </div>
 
-        <div className={style.weapon}>
-          <div className={style.gun_title}>Photon grenade</div>
-
-          <div className={style.weapon_content}>
-            <img src={range} alt="range" className={style.param_img} />
-            <div className={style.gun_number}>12"</div>
+        <div className={style.strat}>
+          <div className={style.stratName_9k}>
+            <span>БЕЗЖАЛОСТНЫЙ ОБСТРЕЛ</span>
+            <span>1 CP</span>
           </div>
-
-          <div className={style.weapon_content}>
-            <div className={style.gun_number}>Gren</div>
-            <div className={style.gun_number}>D6</div>
-          </div>
-
-          <div className={style.weapon_content}>
-            <img src={S} alt="S" className={style.param_img} />
-            <div className={style.gun_number}>-</div>
-          </div>
-
-          <div className={style.weapon_content}>
-            <div className={style.gun_number}>AP</div>
-            <div className={style.gun_number}>-</div>
-          </div>
-
-          <div className={style.weapon_content}>
-            <div className={style.gun_number}>D</div>
-            <div className={style.gun_number}>-</div>
+          <div className={style.stratName_info}>
+            Используйте эту стратегию на этапе стрельбы, когда для стрельбы
+            выбирается подразделение УДАРНОЙ группы из вашей армии. До конца фазы:
+            Вместо того, чтобы следовать обычным правилам для скорострельного оружия,
+            модели в этом подразделении, стреляющие из импульсных винтовок, совершают вдвое больше атак.
+            Каждый раз, когда БАЗОВАЯ модель в этом подразделении совершает атаку дальнего боя,
+            улучшайте характеристику бронепробиваемости этой атаки на 1.
           </div>
         </div>
+
+        <div className={style.strat}>
+          <div className={style.stratName_9k}>
+            <span>ИМПУЛЬСНЫЙ НАТИСК</span>
+            <span>1 CP</span>
+          </div>
+          <div className={style.stratName_info}>
+            Используйте эту стратегию на этапе стрельбы, когда для стрельбы выбирается подразделение
+            FIRE WARRIOR TEAM из вашей армии. До конца фазы, каждый раз, когда БАЗОВАЯ модель в этом
+            подразделении совершает атаку импульсным оружием, неизмененный бросок удара из 6 автоматически
+            ранит цель.
+          </div>
+        </div>
+
+        <div className={style.strat}>
+          <div className={style.stratName_9k}>
+            <span>ЗАЛП В УПОР</span>
+            <span>1 CP</span>
+          </div>
+          <div className={style.stratName_info}>
+            Используйте эту стратегию в начале вашей фазы стрельбы.
+            Выберите одно подразделение FIRE WARRIOR TEAM из вашей армии,
+            которое находится в пределах досягаемости любых вражеских подразделений.
+            До конца фазы импульсные бластеры, импульсные карабины и импульсные винтовки,
+            которыми оснащены модели в этом подразделении, имеют характеристику типа Pistol 2.
+          </div>
+        </div>
+
       </section>
     </div >
   )
