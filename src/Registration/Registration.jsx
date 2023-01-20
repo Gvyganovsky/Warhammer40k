81563f57aa4f3fc5e9c5faf8c1f8a48359ad5e7e
Gvyganovsky <gvyganovsky@gmail.com>
2023-01-20 19:46:57 +0300

    Registration, part 2

--- src/Registration/Registration.jsx
+++ src/Registration/Registration.jsx
@@ -1,16 +1,26 @@
 import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { Form } from "./Form"
 import { setUser } from "../store/slices/userSlice"
 
 const Registration = () => {
     const dispatch = useDispatch();
+    let navigate = useNavigate();
 
     const handleRegister = (email, password) => {
         const auth = getAuth();
         console.log(auth);
         createUserWithEmailAndPassword(auth, email, password)
-            .then(console.log)
+            .then(({ user }) => {
+                console.log(user);
+                dispatch(setUser({
+                    email: user.email,
+                    id: user.uid,
+                    token: user.accessToken,
+                }));
+                navigate('/')
+            })
             .catch(console.error)
     }
 
