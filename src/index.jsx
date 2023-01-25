e02b40c36673d5b29aa1a20945fa59f06a3fe8a6
Gvyganovsky <gvyganovsky@gmail.com>
2023-01-25 20:42:44 +0300

    redux-persist, part 1

--- src/index.jsx
+++ src/index.jsx
@@ -4,17 +4,22 @@ import './index.css';
 import App from './App';
 
 import { Provider } from 'react-redux';
-import { store } from './store';
+import { store } from './store/index';
 import './firebase';
 
 import { BrowserRouter } from 'react-router-dom';
+
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor } from './store/index';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
