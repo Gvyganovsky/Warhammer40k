300f2a752ad8149e99dca2b4891c8f075326f8ac
Gvyganovsky <gvyganovsky@gmail.com>
2023-01-26 20:06:29 +0300

    Log out of the profile

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,13 +1,21 @@
 import { useAuth } from 'hooks/use-auth'
+import { useDispatch } from 'react-redux'
 import { removeUser } from 'store/slices/userSlice'
+import { useNavigate } from "react-router-dom";
+import { useEffect } from 'react'
 
 const Profile = () => {
     const { email } = useAuth();
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     return (
         <section>
             <h1>{email}</h1>
-            
+            <button onClick={() => 
+                dispatch(removeUser(),
+                navigate('/')
+                )}></button>
         </section>
     )
 }
