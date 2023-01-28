a40844963ba58706d224cb0ab993ef7cf558a3b1
Gvyganovsky <gvyganovsky@gmail.com>
2023-01-28 12:24:23 +0300

    style profile

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -2,6 +2,8 @@ import { useAuth } from 'hooks/use-auth'
 import { useDispatch } from 'react-redux'
 import { removeUser } from 'store/slices/userSlice'
 import { useNavigate } from "react-router-dom";
+import styles from '../scss/Profile.module.scss'
+import profile from '../assets/icons/profile.svg'
 
 const Profile = () => {
     const { email } = useAuth();
@@ -9,12 +11,13 @@ const Profile = () => {
     const navigate = useNavigate();
 
     return (
-        <section>
-            <h1>{email}</h1>
-            <button onClick={() => 
+        <section className={styles.profile}>
+            <img src={profile} alt="profile" className={styles.profile_img} />
+            <h1 className={styles.profile_email}>{email}</h1>
+            <button onClick={() =>
                 dispatch(removeUser(),
-                navigate('/')
-                )}></button>
+                    navigate('/')
+                )} className={styles.profile_but}>Выйти из аккаунта</button>
         </section>
     )
 }
