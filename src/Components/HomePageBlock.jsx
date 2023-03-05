d06da251d637bf17df908745831bd8c6fd9d1482
Gvyganovsky <gvyganovsky@gmail.com>
2023-03-05 19:56:19 +0300

    Link HomePage

--- src/Components/HomePageBlock.jsx
+++ src/Components/HomePageBlock.jsx
@@ -1,12 +1,13 @@
+import { Link } from 'react-router-dom';
 import styles from '../scss/HomePage.module.scss';
 
 export const HomePageBlock = (props) => {
     return (
-        <a href={props.link}>
+        <Link to={props.link}>
             <div className={styles.block_conent}>
                 <img src={props.img} alt={props.img} className={styles.content_img} />
                 <div className={styles.content_title}>{props.title}</div>
             </div>
-        </a>
+        </Link>
     )
 }
